a5fd3f44e219ed80185fbfd80cbe0127fe39138f
sumukh-develop <114153077+sumukh-develop@users.noreply.github.com>
2025-06-03 10:22:49 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package prg10.prg10_example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
